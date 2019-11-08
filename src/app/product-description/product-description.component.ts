f01a0f155c1446d9fd96eb70bfdc47ba29ba3392
Roseline Damachi <wadaroselyn@gmail.com>
2019-11-08 14:05:58 +0100

    chore: Fix Product Service

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,11 +7,12 @@ import {ProductService} from '../product.service'
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
+  albumInfo;
+  
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
