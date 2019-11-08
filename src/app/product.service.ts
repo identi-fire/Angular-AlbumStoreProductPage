4c58c781f895ca9734060ac799fbdf30b50b1f8c
Roseline Damachi <wadaroselyn@gmail.com>
2019-11-08 15:08:04 +0100

    chore: Product list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import {Album} from './album';
+import { Album } from './album';
+import { Product } from './product';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 
@@ -10,8 +11,13 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json'
 
   public getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album> response.json());
-  } 
+  }
+
+  public getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 }
